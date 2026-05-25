104d3c5005eb0d0a015e2be62f244af6f06475e1
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-05-25 22:04:13 +0200

    .

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express =require('express');
 const app = express();
 app.get('/',(req,res)=>{
+    // res.send({message:'Hello World'});
     res.send('Hello World');
+});
+app.get('/about',(req,res)=>{
+    res.send('About');
 });
 app.listen(8000,()=>
 {
