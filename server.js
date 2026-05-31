3184278838bbad1d9892e2b35f0a5170ddf5369c
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-05-31 21:26:27 +0200

    .

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express =require('express');
+const path =require('path');
 const app = express();
+
+//setup static  folder
+
+// app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/',(req,res)=>{
     // res.send({message:'Hello World'});
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname,'public','index.html'));
 });
 app.get('/about',(req,res)=>{
-    res.send('About');
+    res.sendFile(path.join(__dirname,'public','about.html'));
 });
 app.listen(8000,()=>
 {
