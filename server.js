b0c55ef5c4997fa3d43a78c1fd3c38bf8cc2dda4
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-05-31 21:29:20 +0200

    .

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const app = express();
 
 //setup static  folder
 
-// app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>{
-    // res.send({message:'Hello World'});
-    res.sendFile(path.join(__dirname,'public','index.html'));
-});
-app.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','about.html'));
-});
+// app.get('/',(req,res)=>{
+//     // res.send({message:'Hello World'});
+//     res.sendFile(path.join(__dirname,'public','index.html'));
+// });
+// app.get('/about',(req,res)=>{
+// //     res.sendFile(path.join(__dirname,'public','about.html'));
+// });
 app.listen(8000,()=>
 {
     console.log(`Server listening on port 8000`);
