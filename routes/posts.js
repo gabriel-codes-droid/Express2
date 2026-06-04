af7c05f327e82319b6191c60297d42f6165e32a5
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-06-04 15:41:55 +0200

    .

--- routes/posts.js
+++ routes/posts.js
@@ -23,10 +23,54 @@ router.get('/:id',(req,res)=>{
     // res.json(posts.filter((posts)=>posts.id===id));
     const post =posts.find((post) => post.id===id);
     if (!post){
-        return res.status(404).json({msg:`A post with the id ${id} was not found`})
+        return res
+          .status(404)
+          .json({msg:`A post with the id ${id} was not found`})
     }
 
         res.status(200).json(post);
     
 });
-module.exports = router;
+//create new post
+router.post('/',(req,res)=>{
+    // console.log(req.body);
+    const newPost={
+        id:posts.length + 1,
+        title:req.body.title
+    };
+    if (!newPost.title){
+        return res.status(400).json({msg:'Please include a title'})
+    }
+    posts.push(newPost);
+
+    res.status(201).json(posts)
+})
+//update post
+router.put('/:id',(req,res)=>{
+    const id =parseInt(req.params.id);
+    const post =posts.find((post)=>post.id===id)
+
+    if(!post){
+        return res
+        .status(404)
+        .json({msg:`A post with id ${id} wasnt found`})
+    }
+
+    post.title =req.body.title;
+    res.status(200).json(posts);
+})
+//delete post
+router.delete('/:id',(req,res)=>{
+    const id =parseInt(req.params.id);
+    const post =posts.find((post)=>post.id===id)
+
+    if(!post){
+        return res
+        .status(404)
+        .json({msg:`A post with id ${id} wasnt found`})
+    }
+
+    posts = posts.filter((post)=>post.id!==id);
+    res.status(200).json({msg:`Post with id ${id} deleted`});
+})
+export default router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express =require('express');
-const path =require('path');
+import express from 'express';
+import path from 'path';
 const app = express();
 const port = process.env.PORT || 8000;
-const posts =require('./routes/posts');
+import posts from './routes/posts.js';
 
 //setup static  folder
 
@@ -15,7 +15,10 @@ const posts =require('./routes/posts');
 // app.get('/about',(req,res)=>{
 // //     res.sendFile(path.join(__dirname,'public','about.html'));
 // });
-//
+//Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended :false}));
+
 app.use('/api/posts',posts);
 app.listen(port,()=>
 {
