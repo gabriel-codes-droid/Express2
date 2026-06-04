10cc9e7042fd5355900eb3436b2878a39bfaa938
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-06-04 21:55:13 +0200

    .

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,13 @@ let posts =[
     {id:1, title:'Post One'},
     {id:2, title:'Post Two'},
     {id:3, title:'Post Three'}
-]
+];
+const logger =(req,res,next)=>{
+    console.log(`
+        ${req.method} ${req.protocol}://${req.get(host)}${ewq.originalURL}`
+     );
+    next();
+}
 //Get all posts 
 router.get('/',(req,res)=>{
     const limit =parseInt(req.query.limit)
@@ -18,14 +24,14 @@ router.get('/',(req,res)=>{
 })
 
 //Get a single post 
-router.get('/:id',(req,res)=>{
+router.get('/:id',(req,res,next)=>{
     const id =parseInt(req.params.id)
     // res.json(posts.filter((posts)=>posts.id===id));
     const post =posts.find((post) => post.id===id);
     if (!post){
         return res
-          .status(404)
-          .json({msg:`A post with the id ${id} was not found`})
+         const error= new Error(`A post with the id ${id} was not found`);
+          return next(error)
     }
 
         res.status(200).json(post);
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 8000;
 import posts from './routes/posts.js';
+import logger from './middleware/logger.js'
+import errorHandler from './middleware/error.js';
 
 //setup static  folder
 
@@ -15,11 +17,20 @@ import posts from './routes/posts.js';
 // app.get('/about',(req,res)=>{
 // //     res.sendFile(path.join(__dirname,'public','about.html'));
 // });
-//Body parser
+//Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended :false}));
 
+//Logger middleware
+
+app.use(logger);
+
+//Routes
 app.use('/api/posts',posts);
+
+//Error handler
+app.use(errorHandler);
+
 app.listen(port,()=>
 {
     console.log(`Server listening on port ${port}`);
