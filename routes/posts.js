a19b1ca3ade9aa712c5381cad35c878b7f778d93
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-06-05 20:34:06 +0200

    .

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {} from '../controllers/postController.js';
+import {getPosts,getPost,createPost,updatePost,deletePost} from '../controllers/postController.js';
 const router = express.Router()
 
 
